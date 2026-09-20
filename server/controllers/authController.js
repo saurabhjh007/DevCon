ad3beafa493a170b7a01c11a7db44a11760e65e0
Saurabh Kumar Jha <saurabh2006jha@gmail.com>
2026-09-20 19:51:59 +0530

    revision, login, jwt

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,48 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+
+const loginController = async (req, res) => {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+        return res.status(400).json({
+            message: "Email and password are required"
+        });
+    }
+
+    const user = await User.findOne({
+        email: email
+    });
+
+    if (!user) {
+        return res.status(401).json({
+            message: "Invalid email or password"
+        });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(
+        password, 
+        user.password
+    )
+
+    if(!isPasswordCorrect){
+        return res.status(401).json({
+            message : "invalid email or password"
+        });
+    }
+
+    const token = jwt.sign(
+        {userId: user._id},
+        process.env.JWT_SECRET,
+        {expiresIn : "7d"}
+    );
+
+    return res.status(200).json({
+        message : "login succesful",
+        token
+    });
+};
 
 const registerController = async (req, res) => {
 
@@ -39,11 +82,33 @@ const registerController = async (req, res) => {
     }
 
     const hashedPassword = await bcrypt.hash(password, 10);
+    console.log("password hashed");
+
+    const newUser = await User.create({
+        name,
+        username,
+        email,
+        password : hashedPassword    
+    });
+
+    console.log("user created");
+
+
     console.log("Hashed password:", hashedPassword);
 
-    res.status(200).json({
-        message: "User validation successful"
+    return res.status(201).json({
+        message: "User registered successful",
+        user : {
+            id : newUser._id,
+            name: newUser.name,
+            username: newUser.username,
+            email: newUser.email
+        }
     });
+
 };
 
-module.exports = registerController;
+module.exports = {
+    registerController,
+    loginController
+};
--- server/routes/authRoute.js
+++ server/routes/authRoute.js
@@ -2,8 +2,12 @@ const express = require("express");
 
 const router = express.Router();
 
-const registerController = require("../controllers/authController");
+const {
+    registerController,
+    loginController
+} = require("../controllers/authController");
 
 router.post("/register", registerController);
+router.post("/login", loginController);
 
 module.exports = router;
